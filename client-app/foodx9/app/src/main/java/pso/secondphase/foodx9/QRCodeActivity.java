d6ff8a11b6436c413c4f5ea5ce4ecc64fda55d77
Vinícius Campos <viniciuscampos120@gmail.com>
2017-11-30 11:31:41 -0300

    Include client-app project

---
+++ client-app/foodx9/app/src/main/java/pso/secondphase/foodx9/QRCodeActivity.java
@@ -0,0 +1,13 @@
+package pso.secondphase.foodx9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QRCodeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+    }
+}
